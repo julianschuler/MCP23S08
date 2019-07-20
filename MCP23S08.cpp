52325ea06017216ceee1e53d8b3334eff122b8af
julianschuler <31921487+julianschuler@users.noreply.github.com>
2019-07-20 22:19:09 +0200

    Initial commit

---
+++ MCP23S08.cpp
@@ -0,0 +1,129 @@
+/**********************************************************************
+ * 
+ * This is the C++ part of the MCP23S08 library.
+ * See MCP23S08.h and the example files for a full documentation.
+ * 
+ *********************************************************************/
+
+
+#include "MCP23S08.h"
+
+
+MCP23S08::MCP23S08(uint8_t csPin, uint8_t deviceAddr) : csPin(csPin) {
+	deviceOpcode = 0x80 | (deviceAddr << 1);
+}
+
+
+void MCP23S08::begin() {
+	SPI.begin();
+	SPI.beginTransaction(settings);
+	pinMode(csPin, OUTPUT);
+	digitalWrite(csPin, LOW);
+	// reset all registers to default:
+	SPI.transfer(MCP23S08_IODIR);	//set address pointer to first register
+	SPI.transfer(0xFF);				// reset first register
+	for (uint8_t i = 0; i < MCP23S08_OLAT; i++) {
+		SPI.transfer(0x00);			// reset other 10 registers
+	}
+	digitalWrite(csPin, LOW);
+	SPI.endTransaction();
+}
+
+
+bool MCP23S08::digitalRead(uint8_t pin) {
+	if (pin > 7) {
+		return 0;
+	}
+	return (getInputStates() >> pin) & 1;
+}
+
+
+void MCP23S08::digitalWrite(uint8_t pin, bool state) {
+	if (pin > 7) {
+		return;
+	}
+	
+	setOutputStates(getOutputStates() & ~(1 << pin) | (state << pin));
+}
+
+
+void MCP23S08::pinMode(uint8_t pin, uint8_t mode) {
+	if (pin > 7) {
+		return;
+	}
+	
+	switch (mode) {
+		case INPUT:
+			setPinModes(getPinModes() & ~(1 << pin));			// set pin to input
+			enablePullups(getEnabledPullups() & ~(1 << pin));	// disable pullup for pin
+			break;
+		case OUTPUT:
+			setPinModes(getPinModes() | (1 << pin));				// set pin to output
+			enablePullups(getEnabledPullups() & ~(1 << pin));	// disable pullup for pin
+			break;
+		case INPUT_PULLUP:
+			setPinModes(getPinModes() & ~(1 << pin));			// set pin to input
+			enablePullups(getEnabledPullups() | (1 << pin));	// enable pullup for pin
+			break;
+	}
+}
+
+
+void MCP23S08::setOutputStates(uint8_t states) {
+	writeRegister(MCP23S08_OLAT, states);
+}
+
+
+void MCP23S08::setPinModes(uint8_t modes) {
+	writeRegister(MCP23S08_IODIR, ~(modes));	// inverted to match IDE defaults
+}
+
+
+void MCP23S08::enablePullups(uint8_t enables) {
+	writeRegister(MCP23S08_GPPU, enables);
+}
+
+
+uint8_t MCP23S08::getInputStates() {
+	return readRegister(MCP23S08_GPIO);
+}
+
+
+uint8_t MCP23S08::getOutputStates() {
+	return readRegister(MCP23S08_OLAT);
+}
+
+
+uint8_t MCP23S08::getPinModes() {
+	return ~(readRegister(MCP23S08_IODIR));		// inverted to match IDE defaults
+}
+
+
+uint8_t MCP23S08::getEnabledPullups() {
+	return readRegister(MCP23S08_GPPU);
+}
+
+
+
+
+void MCP23S08::writeRegister(uint8_t address, uint8_t data) {
+	SPI.beginTransaction(settings);
+	digitalWrite(csPin, LOW);
+	SPI.transfer(deviceOpcode);		// initialize transfer with opcode and R/W-flag cleared
+	SPI.transfer(address);
+	SPI.transfer(data);
+	digitalWrite(csPin, HIGH);
+	SPI.endTransaction();
+}
+
+
+uint8_t MCP23S08::readRegister(uint8_t address) {
+	uint8_t data;
+	SPI.beginTransaction(settings);
+	digitalWrite(csPin, LOW);
+	SPI.transfer(deviceOpcode | 1);		// initialize transfer with opcode and R/W-flag set
+	data = SPI.transfer(address);
+	digitalWrite(csPin, HIGH);
+	SPI.endTransaction();
+	return data;
+}
---
+++ MCP23S08.h
@@ -0,0 +1,66 @@
+/**********************************************************************************************
+ * 
+ * This library for the Arduino IDE allows the simple usage of the MCP23S08 SPI based
+ * I/O port expander by providing functions like digitalWrite, digitalRead and pinMode.
+ * It is licensed under the MIT License, see LICENSE.txt.
+ * 
+ *********************************************************************************************/
+ 
+
+#ifndef __MCP23S08_h__
+#define __MCP23S08_h__
+
+#include <SPI.h>
+
+
+// register addresses
+#define MCP23S08_IODIR		0x00
+#define MCP23S08_IPOL		0x01
+#define MCP23S08_GPINTEN	0x02
+#define MCP23S08_DEFVAL		0x03
+#define MCP23S08_INTCON		0x04
+#define MCP23S08_IOCON		0x05
+#define MCP23S08_GPPU		0x06
+#define MCP23S08_INTF		0x07
+#define MCP23S08_INTCAP		0x08
+#define MCP23S08_GPIO		0x09
+#define MCP23S08_OLAT		0x0A
+
+
+class MCP23S08 {
+public:
+	// constructors
+	MCP23S08(uint8_t csPin) {MCP23S08(csPin, 0);}
+	MCP23S08(uint8_t csPin, uint8_t deviceAddr);
+	
+	// call in setup
+	void begin();
+	
+	// usage equivalent to the default IDE functions, see examples
+	bool digitalRead(uint8_t pin);
+	void digitalWrite(uint8_t pin, bool state);
+	void pinMode(uint8_t pin, uint8_t mode);
+	
+	// advanced, write settings for all pins at once
+	void setOutputStates(uint8_t states);
+	void setPinModes(uint8_t modes);
+	void enablePullups(uint8_t enables);
+	
+	// advanced, get settings for all pin at once
+	uint8_t getInputStates();
+	uint8_t getOutputStates();
+	uint8_t getPinModes();
+	uint8_t getEnabledPullups();
+	
+private:
+	// private variables
+	uint8_t csPin;
+	uint8_t deviceOpcode;
+	const SPISettings settings = SPISettings(10000000, MSBFIRST, SPI_MODE0);
+	
+	// low level SPI communication
+	void writeRegister(uint8_t address, uint8_t data);
+	uint8_t readRegister(uint8_t address);
+};
+
+#endif	// __MCP23S08_h__
